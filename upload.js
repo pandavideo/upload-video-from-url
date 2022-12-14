07b4682c0d455613208ae88b375ee0d8e151b240
Luiz Guilherme <luizgomesdev@gmail.com>
2022-12-14 12:13:41 -0300

    feat: add websocket to retrieve video progress

--- upload.js
+++ upload.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
 const { v4: uuidv4 } = require("uuid");
 
+const WebSocket = require("ws");
+const WebSocketAsPromised = require("websocket-as-promised");
+
 // Header
 const API_KEY = "API_KEY"; // (required)
 
@@ -10,6 +13,9 @@ const video_id = uuidv4(); // (optional) | obs: You can put an especific uuid fo
 const title = "video name"; // (optional) | obs: title is your video name
 const description = "video description"; // (optional)
 const url = "https://.."; // (required) | obs: url is an external video url
+
+// This will console log all the video progress if (true)
+const showVideoProgress = true;
 
 const uploadVideo = async () => {
   try {
@@ -31,10 +37,43 @@ const uploadVideo = async () => {
       }
     );
 
-    console.log(
-      "UPLOAD IN PROGRESS, you can check upload status in:",
-      response.data.websocket_url
-    );
+    const websocket = response.data.websocket_url;
+
+    if (!showVideoProgress) {
+      console.log(
+        "UPLOAD IN PROGRESS, you can check upload status in:",
+        websocket
+      );
+    } else {
+      const webSocketConnect = async (endpoint) => {
+        return new Promise((resolve, reject) => {
+          const wsp = new WebSocketAsPromised(endpoint, {
+            createWebSocket: (url) => new WebSocket(url),
+            extractMessageData: (event) => event,
+          });
+          wsp
+            .open()
+            .then(() => {
+              wsp.onMessage.addListener((data) => {
+                const obj = JSON.parse(data.toString("utf8"));
+                console.log("Upload status: " + obj.action);
+                if (obj.action === "success") {
+                  resolve(true);
+                  wsp.close();
+                } else if (obj.action == "error") {
+                  resolve(false);
+                  wsp.close();
+                }
+              });
+            })
+            .catch((e) => {
+              resolve(false);
+            });
+        });
+      };
+
+      webSocketConnect(websocket);
+    }
   } catch (error) {
     console.log("UPLOAD ERROR:", error.response.data);
   }
